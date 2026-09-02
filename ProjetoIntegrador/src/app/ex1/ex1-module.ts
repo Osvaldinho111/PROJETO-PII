bc4a5078ca30b14ac7c07e273451a93339d56237
Osvaldinho111 <projetoint2026@gmail.com>
2026-09-02 01:01:27 +0000

    ex2

--- ProjetoIntegrador/src/app/ex1/ex1-module.ts
+++ ProjetoIntegrador/src/app/ex1/ex1-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { Ex1RoutingModule } from './ex1-routing-module';
 import { Exercicio1 } from './exercicio1/exercicio-1';
+import { Exercicio2 } from './exercicio2/exercicio2';
 
 @NgModule({
-  declarations: [Exercicio1],
+  declarations: [Exercicio1, Exercicio2],
   imports: [CommonModule, Ex1RoutingModule],
 })
 export class Ex1Module {}
--- ProjetoIntegrador/src/app/ex1/ex1-routing-module.ts
+++ ProjetoIntegrador/src/app/ex1/ex1-routing-module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Exercicio2 } from './exercicio2/exercicio2';
 import { Exercicio1 } from './exercicio1/exercicio-1';
 
 const routes: Routes = [
   {path:'exercicio-1', component: Exercicio1},
+  {path:'exercicio-1', component: Exercicio2},
+
 ];
 
 @NgModule({
